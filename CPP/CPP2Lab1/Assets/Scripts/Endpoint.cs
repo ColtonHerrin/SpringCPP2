5e6d2ae319b0024d192ee735813dc50e8a965de3
ColtonHerrin <coltonherrin42@gmail.com>
2022-04-11 16:47:48 -0400

    Added To Unity

---
+++ CPP/CPP2Lab1/Assets/Scripts/Endpoint.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Endpoint : GameManager
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown (KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+
+    private void OnCollisionEnter(Collision gameObjectInformation)
+    {
+        if (gameObjectInformation.gameObject.name == "Player")
+        {
+            Application.Quit();
+        }
+    }
+}
